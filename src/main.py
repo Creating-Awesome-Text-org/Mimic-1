8406b42c3fa3fc8d3d651e9617c44f2d908dc4a1
Travis Dawson <t.dawson@student.maastrichtuniversity.nl>
2023-08-15 19:43:56 +0200

    FastAPI setup completed. Basic credentials POST request completed. Confirmed on swagger requirements.txt updated with FastAPI

--- src/main.py
+++ src/main.py
@@ -1,5 +1,25 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import CredentialsEnvironment
+
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    print("Mimic 1")
+class Credentials(BaseModel):
+    openai_api_key: str
+    pinecone_api_key: str
+    pinecone_env: str
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to Mimic"}
+
+
+@app.post("/credentials")
+async def mimic_credentials(credentials: Credentials):
+    CredentialsEnvironment.set_credentials(credentials.openai_api_key,
+                                           credentials.pinecone_api_key,
+                                           credentials.pinecone_env)
+    return {"message": "Environmental credentials setup"}
 
