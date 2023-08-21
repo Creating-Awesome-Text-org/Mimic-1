eb629de0c58758464dd8bf534909e5763b4db51c
Travis Dawson <t.dawson@student.maastrichtuniversity.nl>
2023-08-21 17:23:07 +0200

    Solves issue of files being resent when sequential files are sent. Additionally frontend and backend are turned into repositories.

--- frontend/javascript/data_upload.js
+++ frontend/javascript/data_upload.js
@@ -3,63 +3,80 @@ const endpoint_root = "http://127.0.0.1:8000";
 function handleFileUpload() {
     const fileInput = document.getElementById("fileInput");
 
+    // Remove existing event listener, if any
+    fileInput.removeEventListener("change", handleFileInputChange);
+
     fileInput.multiple = true;
     fileInput.click();
 
-    fileInput.addEventListener("change", (event) => {
-        const files = event.target.files;
-
-        if (files.length > 0) {
-            const formData = new FormData();
-            for (const file of files) {
-                formData.append("files", file);
-            }
-
-            fetch(endpoint_root + "/files_upload", {
-                method: "POST",
-                body: formData
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data); // Handle response from server
-            })
-            .catch(error => {
-                console.error("Error:", error);
-            });
-        } else {
-            console.log("No files selected.");
-        }
-    });
+    fileInput.addEventListener("change", handleFileInputChange);
 }
 
+function handleFileInputChange(event) {
+    const files = event.target.files;
+
+    if (files.length > 0) {
+        const formData = new FormData();
+
+        // Clear any existing files in the formData
+        formData.delete("files");
+
+        for (const file of files) {
+            formData.append("files", file);
+        }
+
+        fetch(endpoint_root + "/files_upload", {
+            method: "POST",
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); // Handle response from server
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+    } else {
+        console.log("No files selected.");
+    }
+}
 
 function handleDirectoryUpload() {
     const directoryInput = document.getElementById("directoryInput");
 
+    // Remove existing event listener, if any
+    directoryInput.removeEventListener("change", handleDirectoryInputChange);
+
     directoryInput.click();
 
-    directoryInput.addEventListener("change", async (event) => {
-        const files = event.target.files;
+    directoryInput.addEventListener("change", handleDirectoryInputChange);
+}
 
-        if (files.length > 0) {
-            const formData = new FormData();
-            for (const file of files) {
-                formData.append("files", file);
-            }
+function handleDirectoryInputChange(event) {
+    const files = event.target.files;
 
-            fetch(endpoint_root + "/files_upload", {
-                method: "POST",
-                body: formData
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data); // Handle response from server
-            })
-            .catch(error => {
-                console.error("Error:", error);
-            });
-        } else {
-            console.log("No files selected.");
+    if (files.length > 0) {
+        const formData = new FormData();
+
+        // Clear any existing files in the formData
+        formData.delete("files");
+
+        for (const file of files) {
+            formData.append("files", file);
         }
-    });
+
+        fetch(endpoint_root + "/files_upload", {
+            method: "POST",
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); // Handle response from server
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
+    } else {
+        console.log("No files selected.");
+    }
 }
