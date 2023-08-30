e6e52b65d34856ed06a05be49abbbb642757361d
Travis Dawson <t.dawson@student.maastrichtuniversity.nl>
2023-08-30 11:37:40 +0200

    Q&A document explained with visual examples

--- frontend/javascript/capabilities.js
+++ frontend/javascript/capabilities.js
@@ -13,3 +13,18 @@ document.getElementById("box3").addEventListener("click", function() {
 document.getElementById("box4").addEventListener("click", function() {
   window.location.href = "doc_generation.html";
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const infoButton = document.querySelector('.info-icon');
+    const modalOverlay = document.getElementById('modalOverlay');
+    const closeButton = document.getElementById('closeButton');
+
+    infoButton.addEventListener('click', function() {
+        modalOverlay.style.display = 'flex';
+    });
+
+    closeButton.addEventListener('click', function() {
+        modalOverlay.style.display = 'none';
+    });
+});
