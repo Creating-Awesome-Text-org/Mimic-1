3f99e0a3281f45438803e063c5ca8d504a85d6e4
Travis Dawson <t.dawson@student.maastrichtuniversity.nl>
2023-08-30 10:45:34 +0200

    Index page added and working

---
+++ frontend/javascript/capabilities.js
@@ -0,0 +1,15 @@
+document.getElementById("box1").addEventListener("click", function() {
+  window.location.href = "qa.html";
+});
+
+document.getElementById("box2").addEventListener("click", function() {
+  window.location.href = "credentials.html";
+});
+
+document.getElementById("box3").addEventListener("click", function() {
+  window.location.href = "generative.html";
+});
+
+document.getElementById("box4").addEventListener("click", function() {
+  window.location.href = "doc_generation.html";
+});
